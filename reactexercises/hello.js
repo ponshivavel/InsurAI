dab6ec138b63cf3452f230f1dee3a2f3d68fcb06
PONSHIVAVEL S K <ponshivavelsk@gmail.com>
2025-10-17 15:47:43 +0530

    Add files via upload

---
+++ reactexercises/hello.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Hello from "./reactframe";
+
+function hello() {
+  return (
+    <div>
+      <Hello />
+    </div>
+  );
+}
+
+export default hello;
---
+++ reactexercises/reactframe.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Hello() {
+  return <h1>Hello I am</h1>;
+}
+
+export default Hello;
+
